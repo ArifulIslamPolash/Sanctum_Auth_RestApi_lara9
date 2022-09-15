dbc494848e70ddb99d8c77f7416264d61a14fde8
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-09-15 09:43:46 +0600

    Task model & migration create with user model and migration modified

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
     ];
 
     /**
@@ -41,4 +42,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function task(){
+        return $this->hasMany(Task::class);
+    }
 }
