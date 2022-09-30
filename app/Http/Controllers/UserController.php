bb233a5f9d70758d2aecdc2b6ae6557f93476b1d
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-09-30 20:37:14 +0600

    User registration code write done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Model\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Validator;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required|email',
+            'phone'=>'required',
+            'password'=>'required'
+        ]);
+        if($validator->fails()){
+            return response()->json(['status' => 'failed','validation_errors' => $validator->errors()]);
+        }
+
+        $inputs=$request->all();
+        $inputs['password']=Hash::make($request->password);
+
+        $user = User::create($inputs);
+
+        if (!is_null($user)) {
+            return response()->json(['status' => 'success','message' => 'User registration successfully completed', 'data' => $user]);
+        } else {
+            return response()->json(['status' => 'success','message' => 'User registration failed']);
+        }
+
+
+
+
+
+
+
+
+    }
 }
